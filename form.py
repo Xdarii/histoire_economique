47803372bbb68db0522a93a2db2b1fb62f25b3c6
dianbah <mdb.dian@gmail.com>
2022-12-25 01:14:52 +0100

    update

--- form.py
+++ form.py
@@ -1,23 +1,16 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-
-st.title('Question')
+import time
+st.title("Histoire des faits économiques et sociaux")
 question = pd.read_csv('dataquestionreponse.csv')
-
-print(question.describe())
-print(question.head())
-
-i= np.random.randint(0,60)
-st.markdown(f"### {question.loc[i]['questions']}")
-form = st.form(key='Mes-qyestions')
-name = form.text_input('saisir votre réponse')
-submit = form.form_submit_button('Valider')
-
-st.write('Appuyer sur valider pour voir la reponse')
-
-if submit:
-    st.markdown('## Votre reponse')
-    st.markdown(f'*{name}*')
-    st.write('## La vraie réponse')
-    st.markdown(f"### :green[{question.loc[i]['reponses']}]")
+np.random.seed(0)
+i= st.slider('Question No:',0,60,43)
+N = np.arange(60)
+np.random.shuffle(N)
+name = st.text_input(f"{question.loc[N[i]]['questions']}")
+if not name:
+  st.warning('saisir votre réponse.')
+  st.stop()
+else:
+    st.markdown(f"### :green[{question.loc[N[i]]['reponses']}]")
